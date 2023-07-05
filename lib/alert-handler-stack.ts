a72b4778cab30d6a6737a73b7db9a0456e99d466
Sushan <sushan.shrestha@outcodesoftware.com>
2023-07-05 21:21:45 +0545

    master [New]: Add Event Bridge, Rule, Target to lambda and API gateway via cdk

--- lib/alert-handler-stack.ts
+++ lib/alert-handler-stack.ts
@@ -1,16 +1,79 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { EventBus, Rule } from "aws-cdk-lib/aws-events";
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AlertHandlerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      runtime: Runtime.NODEJS_18_X,
+      // entry:
+    };
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AlertHandlerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // prepare nodejs function
+    const alertFunction = new NodejsFunction(this, "alertLambdaFunction", {
+      entry: join(__dirname, `../src/alert/index.js`),
+      ...nodeJsFunctionProps,
+    });
+
+     //Create a new bus
+     const AlertEventBus = new EventBus(this, 'alertEventBus', {
+      eventBusName: `AlertEventBus`
+    });
+
+    // prepare event rule
+
+    const eventRule = new Rule(this, 'alertEventRule', {
+      eventBus:AlertEventBus,
+      eventPattern:{
+        detailType:[
+          'hello'
+        ]
+      }
+    })
+
+    eventRule.addTarget(new targets.LambdaFunction(alertFunction));
+
+    // prepare event bridge
+    // const alertEventBridge = new EventBridgeDestination()
+
+    // const api = new LambdaRestApi(this, "alertsApi", {
+    //   restApiName: "Alert Service",
+    //   handler: alertFunction,
+    //   proxy: false,
     // });
+
+    const alertApi = new RestApi(this,"alertApi");
+
+    alertApi.root.resourceForPath("/alerts").addMethod("POST", new LambdaIntegration(alertFunction))
+
+    // POST /alerts
+    // PATCH /alerts/{:id}
+    // DELETE /alerts/{:id}
+
+    // const alerts = api.root.addResource("alerts");
+
+    // alerts.addMethod("POST"); // POST /alerts
+
+    // handle single alert
+    // const singleAlert = alerts.addResource(`{id}`); // alerts/{id}
+
+    // singleAlert.addMethod("PATCH"); // PATCH /alerts/{id}
+    // singleAlert.addMethod("DELETE"); // DELETE /alerts/{id}
+
+    
   }
 }
