ab33fb6075e524e096a6a3aa174c6bd6b5ee190a
Sushan <sushan.shrestha@outcodesoftware.com>
2023-07-10 17:15:55 +0545

    add layer and try sending event

--- lib/alert-handler-stack.ts
+++ lib/alert-handler-stack.ts
@@ -1,79 +1,80 @@
 import * as cdk from "aws-cdk-lib";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { EventBus, Rule } from "aws-cdk-lib/aws-events";
-import * as targets from 'aws-cdk-lib/aws-events-targets';
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+import { Runtime, LayerVersion, Code } from "aws-cdk-lib/aws-lambda";
 import {
   NodejsFunction,
   NodejsFunctionProps,
 } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AlertHandlerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create the LayerVersion
+    const myLayer = new LayerVersion(this, "MyLayer", {
+      code: Code.fromAsset("src/zip/nodejs.zip"),
+      compatibleRuntimes: [Runtime.NODEJS_18_X],
+      description: "My custom layer",
+    });
 
     const nodeJsFunctionProps: NodejsFunctionProps = {
       bundling: {
         externalModules: ["aws-sdk"],
       },
       runtime: Runtime.NODEJS_18_X,
-      // entry:
+      environment: {
+        EVENT_SOURCE: "com.apigateway.test",
+        EVENT_DETAILTYPE: "API Gateway Request",
+        EVENT_BUSNAME: "AlertEventBus",
+      },
     };
 
-    // prepare nodejs function
+    // Prepare nodejs function
     const alertFunction = new NodejsFunction(this, "alertLambdaFunction", {
       entry: join(__dirname, `../src/alert/index.js`),
       ...nodeJsFunctionProps,
     });
 
-     //Create a new bus
-     const AlertEventBus = new EventBus(this, 'alertEventBus', {
-      eventBusName: `AlertEventBus`
+    alertFunction.addLayers(myLayer);
+
+    // Prepare nodejs function
+    const destinationFunction = new NodejsFunction(
+      this,
+      "destinationFunction",
+      {
+        entry: join(__dirname, `../src/eventbridge/index.js`),
+        ...nodeJsFunctionProps,
+      }
+    );
+
+    const alertApi = new RestApi(this, "alertApi");
+
+    alertApi.root
+      .resourceForPath("/alerts")
+      .addMethod("POST", new LambdaIntegration(alertFunction));
+
+    // Create a new bus
+    const alertEventBus = new EventBus(this, "alertEventBus", {
+      eventBusName: "AlertEventBus",
     });
 
-    // prepare event rule
+    const eventRule = new Rule(this, "alertEventRule", {
+      eventBus: alertEventBus,
+      enabled: true,
+      description: "When Alert are received check rule and send if passed",
+      eventPattern: {
+        source: ["com.apigateway.test"],
+        detailType: ["API Gateway Request"],
+      },
+      ruleName: "alertEventRule",
+    });
 
-    const eventRule = new Rule(this, 'alertEventRule', {
-      eventBus:AlertEventBus,
-      eventPattern:{
-        detailType:[
-          'hello'
-        ]
-      }
-    })
+    eventRule.addTarget(new targets.LambdaFunction(destinationFunction));
 
-    eventRule.addTarget(new targets.LambdaFunction(alertFunction));
-
-    // prepare event bridge
-    // const alertEventBridge = new EventBridgeDestination()
-
-    // const api = new LambdaRestApi(this, "alertsApi", {
-    //   restApiName: "Alert Service",
-    //   handler: alertFunction,
-    //   proxy: false,
-    // });
-
-    const alertApi = new RestApi(this,"alertApi");
-
-    alertApi.root.resourceForPath("/alerts").addMethod("POST", new LambdaIntegration(alertFunction))
-
-    // POST /alerts
-    // PATCH /alerts/{:id}
-    // DELETE /alerts/{:id}
-
-    // const alerts = api.root.addResource("alerts");
-
-    // alerts.addMethod("POST"); // POST /alerts
-
-    // handle single alert
-    // const singleAlert = alerts.addResource(`{id}`); // alerts/{id}
-
-    // singleAlert.addMethod("PATCH"); // PATCH /alerts/{id}
-    // singleAlert.addMethod("DELETE"); // DELETE /alerts/{id}
-
-    
+    alertEventBus.grantPutEventsTo(alertFunction);
   }
 }
